c1268a65eb1cede37aefdc8fe5cc3a50822685d2
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-07-28 16:46:42 -0300

    Script data_alpha para retirar dados do alphalink

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 from Query_Sql import *
 from manipulation_data import *
+from data_alpha import *
 
 dados = manipulation("humberto.csv")
 
@@ -15,3 +16,11 @@ FROM Dados'''
 
 df = sql_df(query, engine)
 print(df.tail())
+
+# Lista com as tags desejadas
+tags_desejadas = ['tag1', 'tag2', 'tag3']
+
+# Loop principal para executar a cada 1 minuto
+while True:
+    retirar_dados(tags_desejadas)
+    time.sleep(60)  # Aguarda 1 minuto antes da próxima execução
