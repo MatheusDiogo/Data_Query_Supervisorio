0aeae17badb9d4208ebb80a82e55b1b6c09acb2c
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-07-28 05:47:28 -0300

    Testando tipos de armazenamento de dados

---
+++ manipulation_data.py
@@ -0,0 +1,59 @@
+##################################################################################
+#                         Campo de Bibliotecas                                   #
+##################################################################################
+
+import pandas as pd
+import re
+
+def manipulation(arquivo):
+    ##################################################################################
+    #                         Abertura do Arquivo                                    #
+    ##################################################################################
+
+    abre_Arquivo = pd.read_csv(arquivo, encoding='ISO-8859-1', delimiter = ';') #variavel do tipo data frame que guardara o arquivo
+    filename = "teste.csv" #nome do arquivo de saida
+
+    ##################################################################################
+    #                         Inicializando variaveis                                #
+    ##################################################################################
+    NumR = abre_Arquivo['TAG'].eq('Descrição').idxmax()+1 #Numero de linhas para repetição
+    lstIteracao = [] # Variavel do tipo lista que ira conter os conteudos dos blocos de 17 linhas na iteracao
+    listsupervisorio = [] # Variavel do tipo lista que ira conter o conteudo do arquivo .csv
+    newlistsupervisorio = [] # Variavel do tipo lista que ira receber a lista com o conteudo sem caracteres indesejados
+    supervisoriofinal = pd.DataFrame() # Variavel do tipo data frame que ira conter a formatacao final desejada
+
+    ##################################################################################
+    #                         Formatacao do Arquivo                                  #
+    ##################################################################################
+
+    listsupervisorio = abre_Arquivo.values.tolist() # Conversao dos dados do arquivo para armazenar na variavel do tipo lista
+
+    listsupervisorio.insert(0,'TAG') # Inserindo item na primeira posicao da lista, item refere-se ao titulo da TAG do componente dentro do arquivo
+
+    # Laco para exclusao de colchetes e aspas contidos nos elementos da lista
+    newlistsupervisorio = [re.sub('[\[\'\]]', '', str(item)) for item in listsupervisorio]
+
+    # Criação do DataFrame formatado usando pd.concat
+    chunks = [newlistsupervisorio[i:i + NumR] for i in range(0, len(newlistsupervisorio), NumR)]
+    supervisoriofinal = pd.concat([pd.Series(chunk) for chunk in chunks], axis=1, ignore_index=True)
+
+    ##################################################################################
+    #                         Saída do Arquivo                                       #
+    ##################################################################################
+    supervisoriofinal = supervisoriofinal.T
+    supervisoriofinal = pd.DataFrame(supervisoriofinal.drop([0,2]))
+    supervisoriofinal.reset_index(drop = True, inplace = True)
+    supervisoriofinal.at[0, 0] = "DATA"
+
+    # Atribuir nomes das colunas corretamente
+    col_names = supervisoriofinal.iloc[0].tolist()
+    supervisoriofinal.columns = col_names
+
+    # Remover a primeira linha (que continha os nomes das colunas repetidos)
+    supervisoriofinal = supervisoriofinal.iloc[1:].reset_index(drop=True)
+
+    # Atribuir o nome correto à primeira coluna
+    supervisoriofinal.rename(columns={col_names[0]: "DATA"}, inplace=True)
+    
+    supervisoriofinal.to_csv(filename, index=False) #convertendo para planilha excel
+    return supervisoriofinal
--- script.py
+++ script.py
@@ -2,17 +2,8 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 from Query_Sql import *
+from manipulation_data import *
 
-file = "Dados.csv"
+dados = manipulation("humberto.csv")
 
-dados = pd.read_csv(str(file), delimiter =';', encoding='iso-8859-1')
-
-engine = create_engine('sqlite:///supervisorio.db')
-
-dados.to_sql('Dados', engine, if_exists='replace', index=False)
-
-query = '''SELECT DATA, Inversor_Jet, Nivel_Jet, Valvula_Pressao_Reator, Valvula_Vazao_Soda, Vavula_Temp_TC1_Secagem_2
-FROM dados 
-WHERE DATA < 17;'''
-df_produtos = sql_df(query, engine)
-print(df_produtos.tail())
+print(dados)
