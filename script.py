bc745c013b548964da01fd806db94c83236d5208
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-08-02 19:20:44 -0300

    Adding TAGS

--- script.py
+++ script.py
@@ -29,7 +29,14 @@ df = sql_df(query, engine)
 print(df.tail())
 
 # Lista com as tags desejadas
-tags_desejadas = ['tag1', 'tag2', 'tag3']
+tags_desejadas = ['V-1101', 'LI-1101', 'TI-2101', 'V-1201', 'LI-1201',
+                  'TI-2102', 'V-1301', 'LI-1301', 'TI-2103', 'V-2105',
+                  'LI-2105', 'TI-2105', 'V-2106', 'LI-2106', 'TI-2106',
+                  'TI-2104', 'LI-1401', 'LI-3102', 'LI-3101', 'FI-4201',
+                  'FI-4204', 'FI-4202', 'LI-3107', 'LI-3104', 'TI-4303',
+                  'LI-4101', 'PI-4401', 'PI-4402', 'TI-4305', 'TI-4304',
+                  'LI-1401', 'TI-0103', 'LI-6101', 'FI-0201', 'TI-0403',
+                  'PI-0301', 'PI-6401', 'TI-0803', 'TI-0804', 'PI-0302']
 
 # # Loop principal para executar a cada 1 minuto
 # while True:
