8244e4323df62b519d5633b886e5a21e46435d91
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-08-02 20:04:17 -0300

    Recebendo data e hora do sistema

--- data_alpha.py
+++ data_alpha.py
@@ -1,16 +1,21 @@
 import subprocess
 import time
+import datetime
 
 def retirar_dados(tags):
     # Defina os parâmetros do comando al_php
+    nome_arquivo_coleta = 'nome_arquivo_coleta'
     numero_alinkw_ini = 1
-    tags_str = "".join(f'\u00A0"{tag}"\u00A0' for tag in tags)  # Substitua pelas tags desejadas
+    tags_str = "".join(f"{{{tag}}}" for tag in tags)  # Substitua pelas tags desejadas
     data_hora_inicial = "dd1/mm1/yy1 hh1:mm1"  # Substitua pelas datas e horas desejadas
-    data_hora_final = "dd2/mm2/yy2 hh2:mm2"  # Substitua pelas datas e horas desejadas
+    # Obter a data e hora atual
+    data_hora_atual = datetime.datetime.now()
+    # Formatar a data e hora no formato desejado
+    data_hora_final = data_hora_atual.strftime("%d/%m/%y %H:%M")
     intervalo_segundos = 60
 
     # Monta o comando completo
-    comando_al_php = f"al_php {numero_alinkw_ini} hist \"{tags_str}\" {data_hora_inicial} {data_hora_final} {intervalo_segundos}"
+    comando_al_php = f"al_php {numero_alinkw_ini} hist \"{tags_str}\" {data_hora_inicial} {data_hora_final} {intervalo_segundos} {nome_arquivo_coleta}"
     print(comando_al_php)
     # Executa o comando al_php no prompt de comando
     subprocess.run(comando_al_php, shell=True)
